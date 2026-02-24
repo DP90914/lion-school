7d027b47e6919fb68ead9fd2fc104e6424de3e6e
Gustavo <gustavodepaulasilva299@gmail.com>
2026-02-24 20:17:50 -0300

    projeto 80%

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const { createElement } = require("react")
+
 const botoes = document.getElementById("botoes")
-const main = document.getElementById("main")
+const main = document.querySelector("main")
 
 criarBotoes()
 
@@ -10,18 +12,18 @@ criarBotoes()
 
 
 
-async function pegarBotoes(){
+async function pegarBotoes() {
     const url = "https://lion-school-phbo.onrender.com/cursos"
     const response = await (await fetch(url)).json()
     return response
 }
 
-async function criarBotoes(){
+async function criarBotoes() {
     const btn = await pegarBotoes()
     btn.forEach(item => {
         const btnCurso = document.createElement("button")
         btnCurso.textContent = item.sigla
-        btnCurso.addEventListener("click", async ()=>{
+        btnCurso.addEventListener("click", async () => {
             procurarAlunosCurso(item.id, item.nome)
         })
         botoes.appendChild(btnCurso)
@@ -30,7 +32,7 @@ async function criarBotoes(){
     });
 }
 
-async function procurarAlunosCurso(curso, nome){
+async function procurarAlunosCurso(curso, nome) {
     const url = `https://lion-school-phbo.onrender.com/alunos?curso_id=${curso}`
     const response = await (await fetch(url)).json()
     criarAlunosCurso(response, nome)
@@ -38,18 +40,18 @@ async function procurarAlunosCurso(curso, nome){
 }
 function criarAlunosCurso(alunos, turma) {
 
-    main.textContent=""
-    
+    main.textContent = ""
+
     main.classList.remove("main")
     main.classList.add("alunoMain")
-    
+
     const turmaNome = document.createElement("h1")
     turmaNome.textContent = turma
     main.appendChild(turmaNome)
 
     const alunosDiv = document.createElement("div")
-    
-    alunos.forEach(item=>{
+
+    alunos.forEach(item => {
         const alunoCard = document.createElement("div")
         const alunoImg = document.createElement("img")
         const alunoNome = document.createElement("h3")
@@ -60,8 +62,9 @@ function criarAlunosCurso(alunos, turma) {
         alunoCard.appendChild(alunoImg)
         alunoCard.appendChild(alunoNome)
         alunoCard.classList.add("alunoCard")
-        alunoCard.addEventListener("click", ()=>{
-            procurarAlunosDesc(item.id)
+        alunoCard.addEventListener("click", async () => {
+            const aluno = await procurarAlunosDesc(item.id)
+            criarAlunosDesc(aluno)
         })
         alunosDiv.appendChild(alunoCard)
     })
@@ -71,21 +74,47 @@ function criarAlunosCurso(alunos, turma) {
 async function procurarAlunosDesc(id) {
     const url = `https://lion-school-phbo.onrender.com/alunos/${id}`
     const response = await (await fetch(url)).json()
-    criarAlunosDesc()
-    console.log(response)
     return response
 }
 
-function criarAlunosDesc(){
+function criarAlunosDesc(aluno) {
+    main.textContent = ""
 
-    main.textContent=""
-    
     main.classList.add("descAlunoMain")
     main.classList.remove("alunoMain")
 
-    const divAluno = createElement("div")
-    const divNotas = createElement("div")
-    
-    
+    const divAluno = document.createElement("div")
+    const divNotas = document.createElement("div")
 
+
+
+    const alunoImg = document.createElement("img")
+    const alunoNome = document.createElement("h3")
+
+    alunoImg.src = aluno.foto
+    alunoNome.textContent = aluno.nome
+
+    aluno.desempenho.forEach(item => {
+        const valorNota = document.createElement("h3")
+        const valorBarra = document.createElement("div")
+        const valorMateria = document.createElement("h3")
+        const divNota = document.createElement("div")
+
+        valorNota.textContent = item.valorBarra
+        valorMateria.textContent = item.categoria
+
+        divNota.appendChild(valorNota)
+        divNota.appendChild(valorBarra)
+        divNota.appendChild(valorMateria)
+
+        divNotas.appendChild(divNota)
+    });
+
+    divAluno.appendChild(alunoImg)
+    divAluno.appendChild(alunoNome)
+
+    divAluno.classList.add("alunoDiv")
+
+    main.appendChild(divAluno)
+    main.appendChild(divNotas)
 }
